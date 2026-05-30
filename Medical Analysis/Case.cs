39abecb7d64a9b077729b9d8a5faa6edb28bcbbd
Ali Mohamed Ali Hassan <100236390+another4433@users.noreply.github.com>
2026-05-31 00:17:46 +0300

    Files for main class test
    These are the data to be used for test class that is the main class. I will share that class here later.

---
+++ Medical Analysis/Case.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Case
+{
+	private string Allergy, Symptom, Condition, Hospital;
+	private DateTime Discover;
+	private long CID;
+	private bool CureAvailable = false;
+    private static Random Random = new Random();
+    public Case()
+	{
+        Allergy = "NA";
+        Symptom = "NA";
+        Condition = "NA";
+        Hospital = "NA";
+        Discover = DateTime.MinValue;
+        CID = 0;
+    }
+    public Case(string Allergy, string Symptom, string Condition, string Hospital, DateTime Discover)
+    {
+        this.Allergy = Allergy;
+        this.Symptom = Symptom;
+        this.Condition = Condition;
+        this.Hospital = Hospital;
+        this.Discover = Discover;
+        CID = GenerateID();
+    }
+    public string AllergyDetail { get => Allergy; set => Allergy = value; }
+    public string SymptomDetail { get => Symptom; set => Symptom = value; }
+    public string ConditionDetail { get => Condition; set => Condition = value; }
+    public string HospitalDetail { get => Hospital; set => Hospital = value; }
+    public DateTime DiscoverDetail { get => Discover; set => Discover = value; }
+    public long CIDDetail()
+    {
+        return CID;
+    }
+    public bool CureAvailableDetail
+    {
+        get => CureAvailable; set => CureAvailable = value;
+    }
+    public int CalculateCaseAge()
+    {
+        int age = DateTime.Now.Year - Discover.Year;
+        if (DateTime.Now.DayOfYear < Discover.DayOfYear)
+            age--;
+        return age;
+    }
+    public int GenerateID()
+    {
+        return Random.Next(999, 999999999);
+    }
+    public void ShowCase()
+    {
+        Console.WriteLine("Case Details:");
+        Console.WriteLine($"CID: {CIDDetail()}");
+        Console.WriteLine($"Allergy: {AllergyDetail}");
+        Console.WriteLine($"Symptom: {SymptomDetail}");
+        Console.WriteLine($"Condition: {ConditionDetail}");
+        Console.WriteLine($"Hospital: {HospitalDetail}");
+        Console.WriteLine($"Discover Date: {DiscoverDetail.ToShortDateString()}");
+        Console.WriteLine($"Is Cure Available? {(CureAvailableDetail ? "Yes" : "No")}");
+    }
+}
