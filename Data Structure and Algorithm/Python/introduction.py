61cf7c51381ae4283dde4da7a581d950f9e196ab
Ali Mohamed Ali <alimohamedhassan9@gmail.com>
2026-06-28 23:15:46 +0300

    feat(ali): I am opening my project here

---
+++ Data Structure and Algorithm/Python/introduction.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+print("Welcome to my Data Structure and Algorithim project")
+print("I will implement each data structure type and add algorithm in it")
