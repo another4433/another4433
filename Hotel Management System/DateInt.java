7d7ca21c5fb32434178bfb1ce3f7cae78e3f0b9e
Ali Mohamed Ali Hassan <100236390+another4433@users.noreply.github.com>
2026-07-24 17:24:30 +0300

    Delete Hotel Management System directory

--- Hotel Management System/DateInt.java
+++
@@ -1,9 +0,0 @@
-public interface DateInt {
-    void setDay(int day);
-    void setMonth(int month);
-    void setYear(int year);
-    int getDay();
-    int getMonth();
-    int getYear();
-    void display();
-}
