5359ac385ceb81929a20c3513a9dacb95be89013
Ali Mohamed Ali Hassan <100236390+another4433@users.noreply.github.com>
2026-07-24 17:24:09 +0300

    Delete Data Structure and Algorithm/Python directory

--- Data Structure and Algorithm/Python/introduction.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-print("Hello,", name)
-print("Welcome to my Data Structure and Algorithim project")
-print("I will implement each data structure type and add algorithm in it")
